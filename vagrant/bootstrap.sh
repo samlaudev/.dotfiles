0f509d65e80ec62a5c0b6b4b1845321879508ef4
Sam Lau <samlaudev@gmail.com>
2022-04-02 10:09:11 +0800

    vagrant bootstrap.sh install docker

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,14 +3,26 @@
 # Created by Sam Lau on 2021/12/16
 # Copyright © 2021 Sam Lau. All rights reserved.
 
+set -x
+
 # Change ubuntu software sources
 wget https://raw.staticdn.net/samlaudev/.dotfiles/main/vagrant/sources.list -O /etc/apt/sources.list 
 
 # Update system software
-apt-get update && apt-get -y upgrade
+apt-get -y -qq update > /dev/null
+apt-get -y -qq upgrade > /dev/null
 
 # Install development software
-apt-get install -y zsh fzf fd-find ripgrep tldr "linux-tools-$(uname -r)" iotop iftop gdb 
+apt-get install -y -qq zsh fzf fd-find ripgrep tldr "linux-tools-$(uname -r)" iotop iftop gdb > /dev/null
+
+# Install docker
+if [[ ! -f "get-dcoker.sh" ]]; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+fi
+
+if [[ ! -x $(command -v docker) ]]; then
+    sh get-docker.sh 
+fi
 
 # Set timezone and environment
 timedatectl set-timezone Asia/Shanghai
