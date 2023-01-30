28bd15b218cbe1cfb726180b5dd615e25b9997de
Sam Lau <samlaudev@gmail.com>
2023-01-30 14:47:44 +0800

    Revert "add zsh-vi-mode"
    This reverts commit d83340d2b2888855f1c3820ef704f8faa8cc6de8.

--- setup.sh
+++ setup.sh
@@ -73,11 +73,7 @@ fi
 
 # zsh-autosuggestions
 if [[ ! -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
-   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
-if [[ ! -e ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode ]]; then
-   git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+   git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions 
 fi
 
 # tmux
